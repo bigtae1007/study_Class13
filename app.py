9c79ad412e19c5cd92b523788dae413d5408520a
taehyeong park <b_tae96@naver.com>
2022-04-23 18:53:30 +0900

    [add] 맛집 저장소 작성 준비
    html, css, js파일 생성, homepage 링크 추가 및 flask 연결

--- app.py
+++ app.py
@@ -2,29 +2,35 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@Cluster0.faljs.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.second
 
 @app.route('/')
 def home():
     return render_template('homepage.html')
 
+# 박태형
+client_btae = MongoClient('mongodb+srv://test:sparta@Cluster0.faljs.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client_btae.food
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-    doc = {
-        'name': name_receive,
-        'comment': comment_receive
-    }
-    db.users.insert_one(doc)
-    return jsonify({'msg':'응원 완료!'})
+@app.route('/food')
+def food_home():
+    return render_template('food.html')
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    all_users = list(db.users.find({},{'_id':False}))
-    return jsonify(all_users)
+
+# @app.route("/homework", methods=["POST"])
+# def homework_post():
+#     name_receive = request.form['name_give']
+#     comment_receive = request.form['comment_give']
+#     doc = {
+#         'name': name_receive,
+#         'comment': comment_receive
+#     }
+#     db.users.insert_one(doc)
+#     return jsonify({'msg':'응원 완료!'})
+#
+# @app.route("/homework", methods=["GET"])
+# def homework_get():
+#     all_users = list(db.users.find({},{'_id':False}))
+#     return jsonify(all_users)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
