26ab416e74c6c69b3ea8feaee5bb50f4cc55cd89
taehyeong park <b_tae96@naver.com>
2022-04-24 17:03:43 +0900

    [update] 공통 html 수정 슬라이드 메뉴 작성
    공통 html에서 css수정, js로 메뉴 슬라이드 작성

--- food.js
+++ food.js
@@ -1,9 +1,12 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  const sideMenu_Btn =  document.getElementById('sideMenu');
+  const sideMenu_detail = document.getElementById('detailMenu');
+  if (sideMenu_detail.style.width == '180px'){
+    sideMenu_detail.style.width = '0';
+    sideMenu_Btn.innerHTML ='&times';
+    sideMenu_Btn.innerHTML ='&#9776;';
+  }else{
+    sideMenu_detail.style.width = '180px';
+    sideMenu_Btn.innerHTML ='&times';
+  }
 }
