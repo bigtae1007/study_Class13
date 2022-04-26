40252ed06b9261b8dca5d4613efefea8a8b38b11
taehyeong park <b_tae96@naver.com>
2022-04-26 17:01:36 +0900

    [add & update] 맛집 정보 db저장 기능 작성
    식당 이름을 통한 크롤링으로 선택시 해당 주소와 이름으로 db에 저장기능 완료

--- app.py
+++ app.py
@@ -3,30 +3,69 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
+import requests
+from bs4 import BeautifulSoup
+
+
 @app.route('/')
 def home():
     return render_template('homepage.html')
 
 # 박태형
 client_btae = MongoClient('mongodb+srv://test:sparta@Cluster0.faljs.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client_btae.food
+db_tae = client_btae.food
+
 
 @app.route('/food')
 def food_home():
     return render_template('food.html')
 
 
-# @app.route("/homework", methods=["POST"])
-# def homework_post():
-#     name_receive = request.form['name_give']
-#     comment_receive = request.form['comment_give']
-#     doc = {
-#         'name': name_receive,
-#         'comment': comment_receive
-#     }
-#     db.users.insert_one(doc)
-#     return jsonify({'msg':'응원 완료!'})
-#
+@app.route("/food", methods=["POST"])
+def show_food():
+    url_receive = request.form['url_give']
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    places = soup.select('#loc-main-section-root > section > div > div.place_section_content > ul > li')
+    addressList = []
+    nameList = []
+
+    for place in places:
+        name = place.select_one('div._3ZU00._1rBq3 > a:nth-child(1) > div > div > span._3Apve').text
+        address = place.select_one('div._3ZU00._1rBq3 > div._1B9G6 > div > span > a > span._3hCbH').text
+        nameList.append(name)
+        addressList.append(address)
+    doc = {
+        'nameList': nameList,
+        'addressList': addressList
+    }
+    return jsonify(doc)
+
+@app.route("/food_save", methods=["POST"])
+def save_food():
+    option_receive = request.form['option_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+    address_receive = request.form['address_give']
+    name_receive = request.form['name_give']
+    doc = {
+        'name': name_receive,
+        'address': address_receive,
+        'star': star_receive,
+        'comment' : comment_receive,
+        'option' : option_receive
+    }
+    db_tae.food_place.insert_one(doc)
+
+    print(option_receive,star_receive,comment_receive,address_receive,name_receive)
+    return jsonify('저장 완료 ! ')
+    # doc = {
+    #     'name': name_receive,
+    #     'comment': comment_receive
+    # }
+    # db.users.insert_one(doc)
 # @app.route("/homework", methods=["GET"])
 # def homework_get():
 #     all_users = list(db.users.find({},{'_id':False}))
---
+++ static/save_food.js
@@ -0,0 +1,38 @@
+function posting() {
+            let storeName = $('#url').val();
+            let url = `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${storeName}`
+            $.ajax({
+                type: 'POST',
+                url: '/food',
+                data: {url_give: url},
+                success: function (response) {
+                    $('#table').empty();
+                    for (let i = 0; i< response.nameList.length; i++){
+                        let temp_html = `<tr>
+                                            <td class="name_db">${response.nameList[i]}</td>
+                                            <td class="address_db">${response.addressList[i]}</td>
+                                            <td><button onclick="sendAddress(${i})">선택</button></td>
+                                        </tr>`
+                        $('#table').append(temp_html)
+                    }
+                }
+            });
+        }
+
+function sendAddress(num){
+    let option = $('#inputGroupSelect01').val();
+    let star = $('#inputGroupSelect02').val();
+    let comment = $('#comment').val();
+    let address = $('.address_db')[num];
+    let name = $('.name_db')[num];
+    $.ajax({
+                type: 'POST',
+                url: '/food_save',
+                data: {option_give: option, star_give:star , comment_give:comment , address_give:address.innerText, name_give: name.innerText},
+                success: function (response) {
+                    alert(response);
+                    location.reload()
+                }
+            });
+
+}
