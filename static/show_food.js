53a5a3fb0cbeca21f97393e4dc5751b18b7db422
taehyeong park <b_tae96@naver.com>
2022-04-26 21:48:41 +0900

    [update] 분류에 따른 색변경
    음식 분류에 따른 색상 변경

--- static/show_food.js
+++ static/show_food.js
@@ -5,17 +5,49 @@ function showing() {
                 url: '/list_food',
                 data: {},
                 success: function (response) {
-                    console.log(response)
                     for (let i = 0; i < response.length ; i++){
                         let name = response[i].name;
                         let address = response[i].address;
                         let option = response[i].option;
                         let star = '👍'.repeat(response[i].star);
                         let comment = response[i].comment;
+                        function color_class (option) {
+                            let optionColor ;
+                            switch (option) {
+                                case '한식':
+                                    optionColor = 'ko';
+                                    break;
+                                case '양식':
+                                    optionColor = 'us';
+                                    break;
+                                case '중식':
+                                    optionColor = 'ch';
+                                    break;
+                                case '일식':
+                                    optionColor = 'ja';
+                                    break;
+                                case '디저트':
+                                    optionColor = 'de';
+                                    break;
+                                case '카페':
+                                    optionColor = 'ca';
+                                    break;
+                                case '패스트푸드':
+                                    optionColor = 'fast';
+                                    break;
+                                case '동남아':
+                                    optionColor = 'ta';
+                                    break;
+                                case '기타':
+                                    optionColor = 'else';
+                                    break;
+                            }
+                            return optionColor;
+                        }
                         let temp_html = `<div class="card" style="width: 18rem;">
                                             <img src="https://cdn.pixabay.com/photo/2018/06/13/18/20/waves-3473335__480.jpg" class="card-img-top" alt="...">
                                             <div class="card-body">
-                                                <h5 class="card-title">${name} <span>${option}</span></h5>
+                                                <h5 class="card-title">${name} <span class="${color_class(option)}">${option}</span></h5>
                                                 <p class="card-text">${comment}</p>
                                             </div>
                                             <ul class="list-group list-group-flush">
@@ -28,10 +60,27 @@ function showing() {
                                             </div>
                                         </div>`
                         $('#content').append(temp_html);
+                    // }
+                    // let countSpan = $('.card-title > span')
+                    // console.log(countSpan)
+                    // for (let i = 0; i< countSpan.length; i++){
+                    //     switch (countSpan[i].text()) {
+                    //         case '한식':
+                    //             console.log(countSpan[i])
+                    //             break;
+                    //         // case '양식':
+                    //         // case '중식':
+                    //         // case '일식':
+                    //         // case '디저트':
+                    //         // case '카페':
+                    //         // case '패스트푸드':
+                    //         // case '동남아':
+                    //         // case '기타':
+                    //
 
+                        }
                     }
 
-                }
             });
         }
 showing();
