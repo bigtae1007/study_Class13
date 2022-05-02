b3288a1eb5cfcb5a6e259fd4dd0e2651ebadca1a
appreciate87 <103627100+appreciate87@users.noreply.github.com>
2022-05-02 16:20:09 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,29 +2,37 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@Cluster0.faljs.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.second
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.zaev4.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAfile=ca)
+db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('homepage.html')
+    return render_template('index.html')
 
+@app.route('/bikeroad', methods=['GET'])
+def web_bikeroad_get():
+    record_list = list(db.bikeroad.find({}, {'_id': False}))
+    return jsonify({'records':record_list})
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
+@app.route('/bikeroad', methods=['POST'])
+def web_bikeroad_post():
     name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
+    road_receive = request.form['road_give']
+    difficult_receive = request.form['difficult_give']
     doc = {
-        'name': name_receive,
-        'comment': comment_receive
-    }
-    db.users.insert_one(doc)
-    return jsonify({'msg':'응원 완료!'})
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    all_users = list(db.users.find({},{'_id':False}))
-    return jsonify(all_users)
+        'name':name_receive,
+        'road':road_receive,
+        'difficult':difficult_receive
+    }
+
+    db.bikeroad.insert_one(doc)
+
+    return jsonify({'msg':'기록 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
