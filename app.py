f41c4d80f472f50a1a98f56635441ead0eeadcf9
Han Cho <104308647+gaius365@users.noreply.github.com>
2022-05-02 17:16:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -149,6 +149,15 @@ def blog_get():
         print("Error Code:" + rescode)
     return jsonify({'desc':json_desc,'name':name_receive})
 
+@app.route('/golf')
+def home2():
+    return render_template('golf.html')
+
+@app.route("/clubs", methods=["GET"])
+def clubs_get():
+    clubs = list(db.clubs.find({}, {'_id': False}))
+    return jsonify({'clubs': clubs})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
