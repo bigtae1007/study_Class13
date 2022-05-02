9381ec0a13565ece1d56107280af021c78f47e7f
saehwa95 <100126319+saehwa95@users.noreply.github.com>
2022-05-02 17:20:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,35 @@ def web_bikeroad_post():
 def home():
     return render_template('homepage.html')
 
+@app.route('/applefarm')
+def home3():
+    return render_template('applefarm.html')
+
+@app.route('/order', methods=['POST'])
+def order_post():
+    goods_receive = request.form['goods_give']
+    num_receive = request.form['num_give']
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+
+    doc = {
+        'goods':goods_receive,
+        'num':num_receive,
+        'name':name_receive,
+        'address':address_receive,
+        'phone':phone_receive
+    }
+
+    db.applefarm.insert_one(doc)
+
+    return jsonify({'msg': '주문 완료!'})
+
+@app.route('/order', methods=['GET'])
+def order_get():
+    orders_list = list(db.applefarm.find({}, {'_id': False}))
+    return jsonify({'orders': orders_list})
+
 # 박태형
 client_btae = MongoClient('mongodb+srv://test:sparta@Cluster0.faljs.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db_tae = client_btae.food
